7d16589d3201a8f1cce1e42217b169a40d7964c5
llmnd <papendiaye511@gmail@example.com>
2025-12-15 15:46:21 +0000

    build quoi

--- mobile/build/web/flutter_bootstrap.js
+++ mobile/build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"b5990e5ccc5e325fd24f0746e7d6689bbebc7c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4019087525"
+    serviceWorkerVersion: "1298569328"
   }
 });
