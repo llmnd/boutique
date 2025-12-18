6a83bd23d92abf767fe19f9c1833700ebe2c2709
mnllmnd <mnllmnd@outlook.fr>
2025-12-18 07:24:34 +0000

    fix: restore Flutter web config and remove bad index.html

--- mobile/build/web/flutter_bootstrap.js
+++ mobile/build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"b5990e5ccc5e325fd24f0746e7d6689bbebc7c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "268594077"
+    serviceWorkerVersion: "2392935882"
   }
 });
